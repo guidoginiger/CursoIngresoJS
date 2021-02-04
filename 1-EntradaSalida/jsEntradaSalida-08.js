38194ada4e934708cfc4cac65efd1037fa7e11e1
Guido Giniger <ginigerguido@gmail.com>
2021-02-04 12:32:25 -0300

    Nuevo cambio Clase 3

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -18,3 +18,11 @@ function SacarResto()
 
 	alert("El resto entre el dividendo y el divisor es " +resultado);
 }
+
+/*	Paso 1 Identificar variables (let).
+	Paso 2 Declarar las variables y fijarme que me pide el enunciado. 
+	Paso 3 Relacionarlo con el HTML a traves de document.getElementById("...").value; a c/ variable.
+	Paso 4 Agregar la variable parseInt(...); porque estamos hablando de numeros enteros.
+	Paso 5 Declarar la variable resultado (¿cual es la operacion que estoy buscando?).
+	Paso 5 Definir alert("...");
+*/
--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,12 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad
 
-}//FIN DE LA FUNCIÓN
+	edad=document.getElementById("txtIdEdad").value;
+
+	if(edad==15)
+	{
+	alert("Niña Bonita");
+	}
+
+}
