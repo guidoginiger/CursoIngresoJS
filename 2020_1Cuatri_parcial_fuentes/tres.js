2035fd76e8c1a59c3ac5e5a069661a4f6b9f9fb2
Guido G. Giniger <ginigerguido@gmail.com>
2021-03-02 00:33:00 -0300

    Add files via upload

--- 2020_1Cuatri_parcial_fuentes/tres.js
+++ 2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,120 @@
+/*En el ingreso a un viaje en avion nos solicitan nombre , edad, sexo("f" o "m") 
+y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
+	a) El nombre de la persona con mas temperatura.
+	b) Cuantos mayores de edad estan viudos
+	c) La cantidad de hombres que hay solteros o viudos.
+	d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+	e) El promedio de edad entre los hombres solteros.*/
 function mostrar()
 {
-	alert("tres");
+	let nombre
+	let edad
+	let sexo
+	let estadocivil
+	let temperatura
+	let seguir
+	let tempMax;
+	let acumViu = 0;
+	let contViu = 0;
+	let cantidadViu;
+	let cantidadSol;
+	let acumSol = 0;
+	let contSol = 0;
+	let cantidadTedad;
+	let acumTedad = 0;
+	let contTedad = 0;
+	let promedio;
+
+	do{
+
+		nombre= prompt("Ingrese un nombre");
+
+		edad= parseInt(prompt("Ingrese la edad de la persona"));
+
+			while(isNaN(edad)){
+
+				edad= parseInt(prompt("ERROR. Ingrese la edad de la persona"));
+			}
+
+		sexo= prompt("Ingrese sexo f/m");
+
+			while(sexo != "f" && sexo!= "m"){
+
+				sexo= prompt("ERROR. Ingrese sexo f/m");
+			}
+
+		estadocivil= prompt("Ingrese estado civil (soltero/casado/viudo)");
+
+			while(estadocivil != "soltero" && estadocivil != "casado" && estadocivil !="viudo"){
+
+				estadocivil= prompt("ERROR. Ingrese estado civil (soltero/casado/viudo)");
+			}
+
+		temperatura= parseInt(prompt("Ingrese temperatura"));
+
+			while(isNaN(temperatura)){
+
+				temperatura= parseInt(prompt("ERROR. Ingrese temperatura"));
+			}
+	
+	seguir= prompt("¿Quiere agregar a mas pasajeros?");
+
+	}while(seguir == "si")
+
+	if( temperatura < tempMax){
+
+		temperatura = tempMax;
+	}
+
+
+	if( sexo == "m" && estadocivil == "soltero"){
+
+		acumSol += cantidadSol;
+		contSol++;
+	}
+
+	else if( sexo == "m" && estadocivil == "viudo"){
+
+		acumViu += cantidadViu;
+		contViu++;
+
+		if( edad >= 18){
+
+			acumViu += cantidadViu;
+			contViu++;
+		}
+	}
+
+	if(edad >= 60){
+
+		if(temperatura > 38){
+
+			acumTedad+=cantidadTedad;
+			contTedad++;
+		}
+	}
+
+	if( sexo == "m" && estadocivil == "soltero"){
+
+		promedio = edad /cantidadSol;
+	}
+
+
+	document.write(nombre+ " es la persona con mas temperatura con: " +temperatura+ "<br>");
+
+	document.write("Estan viudos: " +contViu+ "<br>");
+
+	document.write("Hay " +contSol+ " solteros y " +contViu+ " viudos <br>");
+
+	document.write("Hay " +contTedad+ " personas de tercera edad que tienen mas de 38 de temperatura <br>");
+
+	document.write("El promedio de edad de hombres solteros es de: " +promedio+ "<br>");
 }
+
+/*En el ingreso a un viaje en avion nos solicitan nombre , edad, sexo("f" o "m") 
+y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
+	a) El nombre de la persona con mas temperatura.
+	b) Cuantos mayores de edad estan viudos
+	c) La cantidad de hombres que hay solteros o viudos.
+	d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+	e) El promedio de edad entre los hombres solteros.*/
--- 2020_1Cuatri_parcial_fuentes/uno.js
+++ 2020_1Cuatri_parcial_fuentes/uno.js
@@ -13,26 +13,115 @@ c) Cuántas unidades de jabones hay en total
 
 function mostrar()
 {
-	let tipo;
-	let precio;
-	let cantidad;
-	let marca;
-	let fabricante;
-	let acumB = 0;
-	let acumJ = 0;
-	let acumA = 0;
-	let contB = 0; 
+	let tipo
+	let precio
+	let cantidad
+	let marca 
+	let fabricante
 	let contJ = 0;
 	let contA = 0;
-	let precioMin;
-	let fabricanteMin;
-	let cantMin;
-	let flagAlcohol = 1;
+	let contB = 0;
+	let acumJ = 0;
+	let acumB = 0;
+	let acumA = 0;
 	let promedio;
+	let maxtipo = 0;
+	let precioMin;
+	let cantidadMin;
+	let fabricanteMin;
+	let flag = 1;
 
-	
+	for(let i=0 ; i<2 ; i++){
 
+		tipo= prompt("Ingrese producto. alcohol o barbijo o jabon");
 
+			while(tipo != "alcohol" && tipo != "barbijo" && tipo!= "jabon"){
+
+				tipo= prompt("ERROR. Ingrese producto. alcohol o barbijo o jabon");	
+			}
+
+		precio= parseInt(prompt("Ingrese un precio entre 100 y 300"));
+
+			while(precio < 100 || precio > 300 || isNaN(precio)){
+
+				precio=parseInt(prompt("ERROR. Ingrese un precio entre 100 y 300"));
+			}
+
+		cantidad= parseInt(prompt("Ingrese cantidad de productos. Entre 1 y 1000"));
+
+			while(cantidad < 1 || cantidad > 1000 || isNaN(cantidad)){
+
+				cantidad= parseInt(prompt("ERROR. Ingrese cantidad de productos. Entre 1 y 1000"));
+			}
+
+		marca= prompt("Ingrese una marca");
+
+		fabricante= prompt("Ingrese un fabricante");
+
+	//a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+
+	if(tipo == "alcohol"){
+
+		if(flag || precio < precioMin ){
+
+			precioMin = precio;
+
+			fabricanteMin = fabricante;
+
+			cantidadMin = cantidad;
+
+		}
+
+		acumA+=cantidad;
+		contA++;
+	}
+
+	//tipo barbijo
+
+	else if( tipo == "barbijo"){
+
+		acumB+=cantidad;
+		contB++;
+	}
+
+	//tipo jabon
+
+	else{
+
+		acumJ+=cantidad;
+		contJ++;
+	}
+}	
+
+	//Del tipo con mas unidades,  el promedio por compra
+
+	if(acumA > acumB && acumA > acumJ){
+
+		promedio = acumA / contA;
+
+		maxtipo = "alcohol";
+
+	}
+
+	else if(acumB >= acumA && acumB >= acumJ){
+
+		promedio = acumB / contB;
+
+		maxtipo = "barbijo";
+	}
+
+	else {
+
+		promedio = acumJ / contJ;
+
+		maxtipo= "jabon";
+	}
+
+	document.write("El alcohol mas barato es el: " +fabricanteMin+ " con " +acumA+ " unidades. Y su precio es: $" +precioMin+  "<br>" );
+
+	document.write("El producto con mas unidades es: " +maxtipo+ " y el promedio por compra es: " +promedio+  "<br>");
+
+	document.write("La cantidad total de jabones es de: " +acumJ+ " unidades <br>");
 
 
 }
